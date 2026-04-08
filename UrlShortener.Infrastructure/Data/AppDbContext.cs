a0ff6e35adb2a1db0d56af532bf445e40ab4b8ff
Võ Minh Tú <vominhtu1212004@gmail.com>
2026-04-08 09:51:28 +0700

    create domain and dockercompose

---
+++ UrlShortener.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Domain.Entities;
+
+
+namespace UrlShortener.Infrastructure.Data
+{
+    public  class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<ShortUrl> ShortUrls { get; set; }
+
+    }
+}
---
+++ UrlShortener.Infrastructure/Repositories/UrlRepository.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace UrlShortener.Infrastructure.Repositories
+{
+    public class UrlRepository : IUrlRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UrlRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(ShortUrl url)
+        {
+            await _context.ShortUrls.AddAsync(url);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<ShortUrl?> GetByCodeAsync(string code)
+        {
+            return await _context.ShortUrls
+                .FirstOrDefaultAsync(x => x.ShortCode == code);
+        }
+    }
+}
